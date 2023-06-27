c4baab3f4ad480990cdc2e7bca749907253958a0
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2023-06-27 12:27:50 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "access_key" {
+        description = "Access key to AWS console"
+}
+variable "secret_key" {
+        description = "Secret key to AWS console"
+}
+
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "DEMO-instance"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+        default = "subnet-XXXXXXXXXXXXXXXX"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+        default = "ami-08e5424edfe926b43"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        default = "Aditya"
+}
