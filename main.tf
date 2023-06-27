8fb28ae9131957b3d92306fba61efff50fab5e08
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2023-06-27 12:26:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "${var.number_of_instances}"
+    subnet_id = "${var.subnet_id}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.ami_key_pair_name}"
+}
